88b7e760a134767f2da1766ddb3f116427477638
paul@inniaccounts.co.uk <55 Yellow rainbow>
2016-03-17 20:59:21 +0000

    Steps are UTF-8 generally, so force it. They certainly aren't ASCII 8 BIT that gherkin returns. Hideous hack!

--- lib/cucumber/rb_support/regexp_argument_matcher.rb
+++ lib/cucumber/rb_support/regexp_argument_matcher.rb
@@ -4,7 +4,7 @@ module Cucumber
   module RbSupport
     class RegexpArgumentMatcher
       def self.arguments_from(regexp, step_name)
-        match = regexp.match(step_name)
+        match = regexp.match(step_name.force_encoding("UTF-8"))
         if match
           n = 0
           match.captures.map do |val|
